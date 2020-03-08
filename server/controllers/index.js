54a458afa683dc6ea840354da84c19d4c5aac6e7
daniel <dela1000@gmail.com>
2020-03-08 20:51:29 +0400

    clean up

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -17,7 +17,6 @@ module.exports = controllers = {
         username: req.body.username,
         password: req.body.password
       };
-      console.log("+++ 20 index.js payload: ", payload)
       if(!payload.username || !payload.password){
         failedResponse(res, "Please submit a username and password")
         return;
