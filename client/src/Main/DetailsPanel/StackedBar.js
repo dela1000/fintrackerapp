a1e388486027fcfe29db99b7c79b302ef2b519a1
daniel <dela1000@gmail.com>
2020-03-10 01:38:46 +0400

    clean up

--- client/src/Main/DetailsPanel/StackedBar.js
+++ client/src/Main/DetailsPanel/StackedBar.js
@@ -11,28 +11,6 @@ import { Chart,
 
 import { capitalize, decimals } from "../../Services/helpers";
 
-const dataSource = [{
-  state: 'Germany',
-  young: 6.7,
-  middle: 28.6,
-  older: 5.1
-}, {
-  state: 'Japan',
-  young: 9.6,
-  middle: 43.4,
-  older: 9
-}, {
-  state: 'Russia',
-  young: 13.5,
-  middle: 49,
-  older: 5.8
-}, {
-  state: 'USA',
-  young: 30,
-  middle: 90.3,
-  older: 14.5
-}];
-
 const customizeTooltip = arg => {
     return {
       text: `${capitalize(arg.seriesName) }: $${decimals(arg.valueText)}`
