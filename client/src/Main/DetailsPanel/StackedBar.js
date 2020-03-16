9c285a1543c02cde79e8f241f4f4ee461399eda7
daniel <dela1000@gmail.com>
2020-03-16 13:32:23 +0400

    clean up

--- client/src/Main/DetailsPanel/StackedBar.js
+++ client/src/Main/DetailsPanel/StackedBar.js
@@ -13,21 +13,27 @@ import {
 
 import { capitalize, decimals } from "../../Services/helpers";
 
-const customizeTooltip = arg => {
-  return { text: `${capitalize(arg.seriesName) }<br>$${decimals(arg.valueText)}<br>Day Total:<br>${decimals(arg.totalText)}` };
-}
-
-const setTitle = (data) => {
-  if(data.type === 'expenses'){
-    if(!data.name){
-      return capitalize(data.type) + " by Day"
-    } else {
-      return "Category " + capitalize(data.name) + " by Day"
-    }
-  }
-}
 
 export default function Stacked ({dailyData, categoryData, viewSelected, argumentField, colors}) {
+  
+  const setTitle = (data) => {
+    if(data.type === 'expenses'){
+      if(!data.name){
+        return capitalize(data.type) + " by Day"
+      } else {
+        return "Category " + capitalize(data.name) + " by Day"
+      }
+    }
+  }
+
+  const customizeTooltip = arg => {
+    if(viewSelected.categoryId){
+      return { text: `${capitalize(arg.seriesName) }<br>$${decimals(arg.valueText)}` };
+    } else {
+      return { text: `${capitalize(arg.seriesName) }<br>$${decimals(arg.valueText)}<br>Day Total:<br>${decimals(arg.totalText)}` };
+    }
+  }
+
   return (
     <Chart
       id="chart"
